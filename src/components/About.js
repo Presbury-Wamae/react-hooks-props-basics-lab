856b4649ef22db8873ac6094c1d19b59c2d5320f
Presbury-Wamae <wamaepresbury034@gmail.com>
2025-04-14 16:01:44 +0300

    Refactor About component to accept props and display bio; add Links component for social links

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(prop) {
+  let bioParagraph;
+
+  if (prop.bio && prop.bio.trim() !== "") {
+    bioParagraph = <p>{prop.bio}</p>;
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bioParagraph}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={prop.github} linkedin={prop.linkedin}/>
     </div>
   );
 }
+
 
 export default About;
---
+++ src/components/Links.js
@@ -0,0 +1,12 @@
+
+
+function Links(prop){
+    return(
+        <>
+        <h3>Links</h3>
+        <a href={prop.github}>https://github.com/liza</a>
+        <a href={prop.linkedin}>https://www.linkedin.com/in/liza/</a>
+        </>
+    )
+}
+export default Links;
